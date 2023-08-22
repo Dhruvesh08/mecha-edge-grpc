c293a5467e1a914bf096e0c92675f073bfbb9651
Dhruvesh08 <dhruveshbaria@gmail.com>
2023-08-22 12:34:36 +0530

    wifi list using grpc

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ mod wifi;
 use wifi::wifi_server::{Wifi, WifiServer};
 use wifi::{Empty, ScanResult, ScanResults};
 
+use mecha_edge_sdk::get_wifi_list;
+
 //wifi struct with default implementation
 #[derive(Default)]
 pub struct WifiImpl {}
@@ -17,16 +19,32 @@ impl Wifi for WifiImpl {
 
         let mut scan_results = ScanResults::default();
 
-        // add 5 scan results
-        for i in 0..5 {
+        //get wifi list from mecha_edge_sdk
+        let wifi_list = get_wifi_list().await.unwrap();
+
+    
+
+        //add wifi list to scan_results
+        for wifi in wifi_list {
             let mut scan_result = ScanResult::default();
-            scan_result.mac = format!("test{}", i);
-            scan_result.frequency = format!("Strong{}", i);
-            scan_result.signal = i;
-            scan_result.flags = format!("wifi{}", i);
-            scan_result.name = format!("jack{}", i);
+            scan_result.mac = wifi.mac;
+            scan_result.frequency = wifi.frequency;
+            scan_result.signal = wifi.signal as i32;
+            scan_result.flags = wifi.flags;
+            scan_result.name = wifi.name;
             scan_results.results.push(scan_result);
         }
+
+        // add 5 scan results
+        // for i in 0..5 {
+        //     let mut scan_result = ScanResult::default();
+        //     scan_result.mac = format!("test{}", i);
+        //     scan_result.frequency = format!("Strong{}", i);
+        //     scan_result.signal = i;
+        //     scan_result.flags = format!("wifi{}", i);
+        //     scan_result.name = format!("jack{}", i);
+        //     scan_results.results.push(scan_result);
+        // }
 
         Ok(Response::new(scan_results))
     }
