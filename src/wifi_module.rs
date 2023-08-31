d907a35d5d46a237d59e4c1ae7abba2a19248424
Dhruvesh08 <dhruveshbaria@gmail.com>
2023-09-01 02:51:27 +0530

    wifi module file changes

--- src/wifi_module.rs
+++ src/wifi_module.rs
@@ -1,20 +1,86 @@
-use mecha_edge_sdk::wifi::WifiModule;
-use tonic::{Request, Response, Status};
-
 use crate::wifi::{
     wifi_server::Wifi, Empty, NetworkResult, NetworkResults, RemoveNetworkRequest,
     RemoveNetworkResponse, ScanResult, ScanResults, WifiConnectRequest, WifiConnectResponse,
 };
 
-//wifi struct with default implementation
+use mecha_edge_sdk::wifi::WifiModule;
+use tonic::{Request, Response, Status};
+
+const WIFI_CONNECT_SUCCESS_MESSAGE: &str = "WiFi connection successful";
+const WIFI_CONNECT_FAILURE_MESSAGE: &str = "WiFi connection failed";
+const NETWORK_REMOVAL_SUCCESS_MESSAGE: &str = "WiFi network removed successfully";
+const NETWORK_REMOVAL_FAILURE_MESSAGE: &str = "WiFi network removal failed";
+
 #[derive(Default)]
 pub struct WifiService {}
+trait ResponseMessage {
+    fn set_success(&mut self, success: bool);
+    fn set_message(&mut self, message: String);
+}
 
-//implement the trait
+impl ResponseMessage for WifiConnectResponse {
+    fn set_success(&mut self, success: bool) {
+        self.success = success;
+    }
+
+    fn set_message(&mut self, message: String) {
+        self.message = message;
+    }
+}
+
+impl ResponseMessage for RemoveNetworkResponse {
+    fn set_success(&mut self, success: bool) {
+        self.success = success;
+    }
+
+    fn set_message(&mut self, message: String) {
+        self.message = message;
+    }
+}
+
+impl WifiService {
+    fn handle_response<T: ResponseMessage>(
+        &self,
+        result: Result<(), &str>,
+        response: &mut T,
+        success_message: &str,
+        failure_message: &str,
+    ) {
+        match result {
+            Ok(_) => {
+                response.set_success(true);
+                response.set_message(success_message.to_string());
+            }
+            Err(_) => {
+                response.set_success(false);
+                response.set_message(failure_message.to_string());
+            }
+        }
+    }
+
+    async fn connect_to_wifi(&self, ssid: &str, psk: &str) -> Result<(), &str> {
+        let connect_wifi = WifiModule::get_connect_wifi(ssid, psk).await;
+
+        match connect_wifi {
+            Ok(_) => Ok(()),
+            Err(_) => Err(WIFI_CONNECT_FAILURE_MESSAGE),
+        }
+    }
+
+    async fn remove_wifi_network(&self, network_id: usize) -> Result<(), &str> {
+        let remove_network = WifiModule::remove_wifi_network(network_id).await;
+
+        match remove_network {
+            Ok(_) => Ok(()),
+            Err(_) => Err(NETWORK_REMOVAL_FAILURE_MESSAGE),
+        }
+    }
+}
+
 #[tonic::async_trait]
-
 impl Wifi for WifiService {
     async fn get_wifi(&self, _request: Request<Empty>) -> Result<Response<ScanResults>, Status> {
+        // Implement your async get_wifi logic here
         let mut scan_results = ScanResults::default();
 
         log::info!("Starting All Wifi List Function");
@@ -41,6 +107,7 @@ impl Wifi for WifiService {
         &self,
         _request: Request<Empty>,
     ) -> Result<Response<NetworkResults>, Status> {
+        // Implement your async get_known_wifi logic here
         let mut scan_results = NetworkResults::default();
         log::info!("Starting Known Wifi List Function");
 
@@ -64,29 +131,15 @@ impl Wifi for WifiService {
         request: Request<WifiConnectRequest>,
     ) -> Result<Response<WifiConnectResponse>, Status> {
         let mut wifi_connect_response = WifiConnectResponse::default();
-
-        log::info!("Starting Wifi Connect Function");
-
         let request_data = request.into_inner();
 
-        //get ssid and psk from request
-        let ssid = request_data.ssid;
-        let psk = request_data.psk;
-
-        //get get_connect_wifi from mecha_edge_sdk
-        //get_connect_wifi  accepts ssid and psk as parameter
-        let connect_wifi = WifiModule::get_connect_wifi(&ssid, &psk).await;
-
-        match connect_wifi {
-            Ok(_) => {
-                wifi_connect_response.success = true;
-                wifi_connect_response.message = "WiFi connection successful".to_string();
-            }
-            Err(_) => {
-                wifi_connect_response.success = false;
-                wifi_connect_response.message = "WiFi connection failed".to_string();
-            }
-        }
+        self.handle_response(
+            self.connect_to_wifi(&request_data.ssid, &request_data.psk)
+                .await,
+            &mut wifi_connect_response,
+            WIFI_CONNECT_SUCCESS_MESSAGE,
+            WIFI_CONNECT_FAILURE_MESSAGE,
+        );
 
         Ok(Response::new(wifi_connect_response))
     }
@@ -95,31 +148,16 @@ impl Wifi for WifiService {
         &self,
         request: Request<RemoveNetworkRequest>,
     ) -> Result<Response<RemoveNetworkResponse>, Status> {
-        log::info!("Starting Remove Network Function");
         let mut remove_network_response = RemoveNetworkResponse::default();
-
-        //get wifi list from mecha_edge_sdk
-        log::info!("Starting Remove Network Function");
-
         let request_data = request.into_inner();
 
-        //get network_id from request
-        let network_id = request_data.network_id;
-
-        //get remove_wifi_network from mecha_edge_sdk
-        //remove_wifi_network accepts network_id as parameter
-        let remove_network = WifiModule::remove_wifi_network(network_id as usize).await;
-
-        match remove_network {
-            Ok(_) => {
-                remove_network_response.success = true;
-                remove_network_response.message = "WiFi network removed successfully".to_string();
-            }
-            Err(_) => {
-                remove_network_response.success = false;
-                remove_network_response.message = "WiFi network removal failed".to_string();
-            }
-        }
+        self.handle_response(
+            self.remove_wifi_network(request_data.network_id as usize)
+                .await,
+            &mut remove_network_response,
+            NETWORK_REMOVAL_SUCCESS_MESSAGE,
+            NETWORK_REMOVAL_FAILURE_MESSAGE,
+        );
 
         Ok(Response::new(remove_network_response))
     }
